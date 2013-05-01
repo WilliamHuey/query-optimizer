56bbd8b26be2759c351cefef53031d6f5f2852e8
Lance Pollard <lancejpollard@gmail.com>
2013-05-01 04:08:07 -0700

    add validator from query

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  */
 
 var Topology = require('tower-topology').Topology
+  , stream = require('tower-stream')
   , adapter;
 
 /**
@@ -45,17 +46,52 @@ var execute = function(query, fn) {
  */
 
 function exec(query, fn) {
-  // XXX: only support one adapter for now.
-  var _adapter = query.adapters[0] || 'memory';
-  // this.validate();
-  // @see http://infolab.stanford.edu/~hyunjung/cs346/ioannidis.pdf
-  // var plan = require('tower-query-plan');
-  // plan(this).exec()
-  // this.validate().plan().exec();
-  // optimize(this).exec();
-  // 
-  // XXX: do validations right here before going to the adapter.
-  return adapter(_adapter).exec(query.criteria, fn);
+  validate(query, function(err){
+    if (err) return fn(err);
+
+    // XXX: only support one adapter for now.
+    var _adapter = query.adapters[0] || 'memory';
+    // this.validate();
+    // @see http://infolab.stanford.edu/~hyunjung/cs346/ioannidis.pdf
+    // var plan = require('tower-query-plan');
+    // plan(this).exec()
+    // this.validate().plan().exec();
+    // optimize(this).exec();
+    // 
+    // XXX: do validations right here before going to the adapter.
+    return adapter(_adapter).exec(query.criteria, fn);
+  });
+}
+
+/**
+ * Add validations to perform before this is executed.
+ *
+ * XXX: not implemented.
+ */
+
+function validate(query, fn) {
+  // XXX: only supports one action at a time atm.
+  query.errors = [];
+  var criteria = query.criteria;
+  var action = criteria[criteria.length - 1][1];
+  var ctx = query;
+  // XXX: collect validators for model and for each attribute.
+  // var modelValidators = model(criteria[0][1].ns).validators;
+  for (var i = 0, n = criteria.length; i < n; i++) {
+    if ('constraint' !== criteria[i][0]) continue;
+
+    var constraint = criteria[i][1];
+
+    if (stream.exists(constraint.left.ns + '.' + action)) {
+      var _action = stream(constraint.left.ns + '.' + action);//.params;
+      var params = _action.params;
+      if (params[constraint.left.attr]) {
+        params[constraint.left.attr].validate(ctx, constraint);
+      }
+    }
+  }
+  // return query.push('validate', fn);
+  query.errors.length ? fn(query.errors) : fn();
 }
 
 // old
